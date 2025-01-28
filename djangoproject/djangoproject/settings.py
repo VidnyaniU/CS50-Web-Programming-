d36059f126c41c2022023c9bc1e9d0360229ee71
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2025-01-28 19:14:54 +0530

    updated django project

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hbdn3_!gnezpj2=w08mh+@0emcj(rs$gl12j-ef@3!#2h$z&x0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pizza',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
